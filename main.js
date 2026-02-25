d97bf7610de0e17eb94ab91b6a96fbc57263771c
JuanMa gToro <torito@hazmeweb.es>
2026-02-25 12:00:55 +0100

    upload data by form

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 
-const ratingButtons = [...document.querySelectorAll(".rating-btn")];
+const ratingForm = document.getElementById("rating-form");
+const ratingCard = document.querySelector(".card-rating");
+const thanksCard = document.getElementById("thanks-card");
+const selectedScoreEl = document.getElementById("selected-score");
+
+ratingForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(ratingForm);
+  const selectedScore = formData.get("score");
+  if (!selectedScore) return;
+
+  selectedScoreEl.textContent = selectedScore;
+  ratingCard.classList.add("is-hidden");
+  thanksCard.classList.remove("is-hidden");
+});
